e3de33573fbb166d46f6041181d8d5caedf848b8
Santiago <santiago.orlando@endava.com>
2022-06-01 17:56:59 -0300

    Make function (not custom command) in webdriveruni-dataTables

--- cypress/integration/webdriver-uni/data-tables.spec.js
+++ cypress/integration/webdriver-uni/data-tables.spec.js
@@ -1,5 +1,3 @@
-//CREATE FUNCTION FOR BOTH IT() GIVEN ITS REPETITIVE
-
 /// <reference types="Cypress" />
 describe("Handling data via webdriveruni", () => {
 
@@ -7,12 +5,40 @@ describe("Handling data via webdriveruni", () => {
         cy.visit("/Data-Table/index.html");
     });
 
+
+    //OPTION 0: create a custom command. It worked, but as the only spec I would be using the function is this one,
+    // it made no sense, and it is more efficient to make a function. Custom commands are work well when I need them for all my tests
+
+
+    //OPTION 1: make a function with a promise. The following warning is displayed on Cypress:
+
+    // While this works in practice, it's often indicative of an anti-pattern. 
+    // You almost never need to return both a promise and also invoke cy commands.
+    // Cy commands themselves are already promise like, and you can likely avoid the use of the separate Promise.
+
+    // function getNumberOfColumn(column) {
+    //     return new Promise((resolve, reject) => {
+    //         cy.get('#thumbnail-1').contains('th', column).invoke('index').then(campaign => {
+    //             resolve(campaign+1);
+    //         });
+    //     });
+    // }
+
+
+    //OPTION 2: make a JS function   //https://docs.cypress.io/api/cypress-api/custom-commands#Best-Practices
+    function getNumberOfColumn(column) {    //or also:  const getNumberOfColumn = (column) => {
+        // return cy chain here so we can chain off this function below
+        return cy.get('#thumbnail-1').contains('th', column).invoke('index').then((i) => { //this brings the index of the column
+            return i + 1;
+        })
+    }
+
+
     it("Calculate and assert the total age of all users", () => {
         let totalAge = 0
 
-        cy.get('#thumbnail-1').contains('th', 'Age').invoke('index').then((i) => { //this brings the index of the column Age.
-            let indexOfAge = i + 1;
-            cy.get('#thumbnail-1 td:nth-child(' + indexOfAge + ')').each(($el, index, $list) => {
+        return getNumberOfColumn('Age').then((i) => { //this brings the index of the column Age.
+            cy.get('#thumbnail-1 td:nth-child(' + i + ')').each(($el, index, $list) => {
                 totalAge += Number($el.text())
             }).then(() => { //thats the solution! To put a then() next to the each
                 expect(totalAge).to.eq(322)
@@ -21,14 +47,13 @@ describe("Handling data via webdriveruni", () => {
     });
 
     it("Calculate and assert the age of a given user based on last name", () => {
-        cy.get('#thumbnail-1').contains('th', 'Lastname').invoke('index').then((i) => { //this brings the index of the column Lastname.
-            let indexOfLastname = i + 1;
+        return getNumberOfColumn('Lastname').then((i) => { //this brings the index of the column Lastname.
             
             //option 1
-            // cy.get('#thumbnail-1 td:nth-child(' + indexOfLastname + ')').each(($el, index, $list) => {
+            // cy.get('#thumbnail-1 td:nth-child(' + i + ')').each(($el, index, $list) => {
             //     const text = $el.text()
             //     if(text.includes("Woods")) {
-            //         cy.get('#thumbnail-1 td:nth-child(' + indexOfLastname + ')').eq(index).next().then((age) => {
+            //         cy.get('#thumbnail-1 td:nth-child(' + i + ')').eq(index).next().then((age) => {
             //             const userAge = age.text();
             //             expect(userAge).to.equal("80");
             //         })
@@ -36,7 +61,7 @@ describe("Handling data via webdriveruni", () => {
             // })
             
             //option 2
-            cy.get('#thumbnail-1 td:nth-child(' + indexOfLastname + ')').contains('Woods').next().invoke('text').should('eq', '80')
+            cy.get('#thumbnail-1 td:nth-child(' + i + ')').contains('Woods').next().invoke('text').should('eq', '80')
         })
     }); 
 
